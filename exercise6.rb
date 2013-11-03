d31248378f076d2eb43d389ec8220b4a20b619c0
noneks <kirill_lp@list.ru>
2013-11-03 21:18:22 +0700

    String and Text

---
+++ exercise6.rb
@@ -0,0 +1,20 @@
+x = "There are #{10} types of people"
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said : #{x}."
+puts "A also said: #{y}."
+
+hilarious = false
+joke_evo = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evo
+
+w = "This is the left side of..."
+e = "a string with a right side"
+
+puts w+e
