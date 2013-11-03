cacebf962504aef22957c919b170a25d485345a9
noneks <kirill_lp@list.ru>
2013-11-04 00:26:59 +0700

    STOP PRINTIGgit add exersice10.rbgit add exersice10.rbgit add exersice10.rbgit add exersice10.rbgit add exersice10.rb

---
+++ exersice10.rb
@@ -0,0 +1,15 @@
+tabby_cat ="\t T'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cay = "I'm \\ a \\ cat. "
+
+fat_cat = <<MY_HEREDOC
+I'll to do a list :
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t*Grass
+MY_HEREDOC
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cay
+puts fat_cat
