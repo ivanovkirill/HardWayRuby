f29a7031db99f926b976d256e47304cee58ef481
noneks <kirill_lp@list.ru>
2013-11-03 22:08:03 +0700

    Printing, printing, printing

---
+++ exercise9.rb
@@ -0,0 +1,12 @@
+days = "Mon Tue Wen Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months: ", months
+
+puts <<PARAGRAF
+Theres something going on here.
+With the PARAGRAF thing
+Well be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAF
